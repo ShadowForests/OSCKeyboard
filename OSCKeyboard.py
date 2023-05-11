24fe9ec3205bcb093d7c61206a5e9adb835412b1
ShadowForests <38903248+ShadowForests@users.noreply.github.com>
2023-05-11 11:04:46 -0700

    Initial socket only implementation

--- OSCKeyboard.py
+++ OSCKeyboard.py
@@ -1,51 +1,108 @@
 # OSC Keyboard by ShadowForest
 # https://github.com/ShadowForests/OSCKeyboard
 
+# Copyright (c) 2022 ShadowForest
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+__version__ = "2.0.0"
+
 from win32gui import GetWindowText, GetForegroundWindow
+from threading import Event, Thread
 from pythonosc import udp_client
+from tkinter import Tk
+import unicodedata
+import pyperclip
+import datetime
 import keyboard
 import json
 import time
+
+from engineio.async_drivers import gevent
+from flask_socketio import SocketIO, emit
+from flask import Flask, request
+
+class SingleThread(Thread):
+    def __init__(self, callback, event):
+        Thread.__init__(self)
+        self.stop = event
+        self.callback = callback
+        self.daemon = True
+
+    def run(self):
+        if not self.stop:
+            try:
+                self.callback()
+            except:
+                pass
 
 class OSCKeyboard:
     def __init__(self, config_file=""):
         self.load_default_config()
         if config_file != "":
-            with open(config_file, "r") as f:
-                config = json.load(f)
+            with open(config_file, "r", encoding="utf-8") as file:
+                config = json.load(file)
                 # OSC IP
                 self.ip = self.load_config_entry(self.ip, config, "ip")
                 # OSC Port
                 self.port = self.load_config_entry(self.port, config, "port")
-                # Require focusing on the window for inputs
-                self.require_focus = self.load_config_entry(self.require_focus, config, "require_focus")
                 # Target window to restrict inputs in
                 self.window = self.load_config_entry(self.window, config, "window")
-                # Delay between key presses
-                self.key_delay = self.load_config_entry(self.key_delay, config, "key_delay")
-                # Key to toggle keyboard
-                self.keyboard_key = self.load_config_entry(self.keyboard_key, config, "keyboard_key")
-                # Key to toggle typing mode
-                self.typing_mode_key = self.load_config_entry(self.typing_mode_key, config, "typing_mode_key")
-                # Key to toggle text mirror
-                self.mirror_key = self.load_config_entry(self.mirror_key, config, "mirror_key")
-                # Key to clear text
-                self.enter_key = self.load_config_entry(self.enter_key, config, "enter_key")
-                # Mapping of key to parameter value
-                self.key_mapping = self.load_config_entry(self.key_mapping, config, "key_mapping")
-                # OSC path prefix
-                self.osc_prefix = self.load_config_entry(self.osc_prefix, config, "osc_prefix")
-                # OSC keyboard parameter name
-                self.keyboard_param = self.load_config_entry(self.keyboard_param, config, "keyboard_param")
-                # OSC key parameter names
-                self.key_params = self.load_config_entry(self.key_params, config, "key_params")
+                # Enables a transcript log of keyboard messages
+                self.transcript = self.load_config_entry(self.transcript, config, "transcript")
+                # Send text immediately instead of opening the keyboard and populating with text
+                self.send_immediate = self.load_config_entry(self.send_immediate, config, "send_immediate")
+                # Enables a socket server connection to allow external input to the keyboard
+                self.socket_connection = self.load_config_entry(self.socket_connection, config, "socket_connection")
+                # Socket IP
+                self.socket_ip = self.load_config_entry(self.socket_ip, config, "socket_ip")
+                # Socket Port
+                self.socket_port = self.load_config_entry(self.socket_port, config, "socket_port")
+                # Key to enable keyboard
+                self.enable_keyboard_key = self.load_config_entry(self.enable_keyboard_key, config, "enable_keyboard_key")
+                # Key to disable keyboard
+                self.disable_keyboard_key = self.load_config_entry(self.disable_keyboard_key, config, "disable_keyboard_key")
+                # OSC chatbox input path
+                self.osc_chatbox_input = self.load_config_entry(self.osc_chatbox_input, config, "osc_chatbox_input")
+                # OSC chatbox typing path
+                self.osc_chatbox_typing = self.load_config_entry(self.osc_chatbox_typing, config, "osc_chatbox_typing")
+
+        warnings = []
+
+        keyboard._winkeyboard._setup_name_tables()
+        if self.enable_keyboard_key not in keyboard._winkeyboard.from_name.keys():
+            warnings.append(f"[WARN] Enable keyboard key '{self.enable_keyboard_key}' was not found in valid keys and may not work!")
+
+        if self.enable_keyboard_key not in keyboard._winkeyboard.from_name.keys():
+            warnings.append(f"[WARN] Disable keyboard key '{self.disable_keyboard_key}' was not found in valid keys and may not work!")
+
+        if len(warnings) > 0:
+            print("\n".join(warnings) + "\n-----------------------------------------------------------")
 
         self.client = udp_client.SimpleUDPClient(self.ip, self.port)
         self.keyboard_enabled = False
-        self.typing_mode_enabled = True
-        self.mirror_enabled = False
-        self.input_hook = None
-        self.enabler_hook = None
+        self.input_window_name = "OSCKeyboard - Text Input Listener"
+        self.text = ""
+
+        # Threads
+        self.socket_stop_flag = None
+        self.socket_thread = None
 
     def load_config_entry(self, entry, config, entry_name):
         if entry_name in config:
@@ -55,166 +112,103 @@ class OSCKeyboard:
     def load_default_config(self):
         self.ip = "127.0.0.1"
         self.port = 9000
-        self.require_focus = True
+
         self.window = "VRChat"
-        self.key_delay = 0.25
-        self.keyboard_key = "="
-        self.typing_mode_key = "-"
-        self.mirror_key = "]"
-        self.enter_key = "enter"
-        self.key_mapping = {
-            "0": 16,
-            "1": 17,
-            "2": 18,
-            "3": 19,
-            "4": 20,
-            "5": 21,
-            "6": 22,
-            "7": 23,
-            "8": 24,
-            "9": 25,
-            "a": 33,
-            "b": 34,
-            "c": 35,
-            "d": 36,
-            "e": 37,
-            "f": 38,
-            "g": 39,
-            "h": 40,
-            "i": 41,
-            "j": 42,
-            "k": 43,
-            "l": 44,
-            "m": 45,
-            "n": 46,
-            "o": 47,
-            "p": 48,
-            "q": 49,
-            "r": 50,
-            "s": 51,
-            "t": 52,
-            "u": 53,
-            "v": 54,
-            "w": 55,
-            "x": 56,
-            "y": 57,
-            "z": 58,
-            "space": 95,
-            "backspace": 254,
-            "enter": 255
-        }
-        self.osc_prefix = "/avatar/parameters/"
-        self.keyboard_param = "Keyboard"
-        self.key_params = [
-            "KeyboardLetterSync1",
-            "KeyboardLetterSync2"
-        ]
+        self.transcript = True
+        self.send_immediate = True
+
+        self.voice_cutoff_time = 3
+        self.socket_connection = True
+        self.socket_ip = "127.0.0.1"
+        self.socket_port = 3000
+
+        self.enable_keyboard_key = "="
+        self.disable_keyboard_key = "esc"
+        self.osc_chatbox_input = "/chatbox/input"
+        self.osc_chatbox_typing = "/chatbox/typing"
 
     def is_correct_window(self):
         if not self.require_focus:
             return True
-        return GetWindowText(GetForegroundWindow()).lower() == self.window.lower()
+        foreground_window = GetWindowText(GetForegroundWindow()).lower()
+        return foreground_window == self.window.lower() or foreground_window == self.input_window_name.lower()
 
-    def enable_keyboard(self):
-        if self.mirror_enabled:
-            self.client.send_message(self.osc_prefix + self.keyboard_param, 12)
-        else:
-            self.client.send_message(self.osc_prefix + self.keyboard_param, 1)
-        time.sleep(self.key_delay)
-
-    def disable_keyboard(self):
-        self.client.send_message(self.osc_prefix + self.keyboard_param, 0)
-        time.sleep(self.key_delay)
-
-    def press_enable(self, key_name):
-        if not self.typing_mode_enabled and self.press_disable(key_name):
-            # Disable keyboard and typing mode
+    def print_transcript_line(self, text=None):
+        if not self.transcript:
             return
 
-        if self.keyboard_enabled and key_name == self.typing_mode_key and not self.typing_mode_enabled:
-            # Toggle typing mode
-            self.typing_mode_enabled = True
-            self.input_hook = keyboard.hook(self.handle_inputs, suppress=True)
-            keyboard.restore_state([]) # Release any held keys
-        elif not self.keyboard_enabled and key_name == self.keyboard_key:
-            # Enable keyboard
-            self.input_hook = keyboard.hook(self.handle_inputs, suppress=True)
-            self.enable_keyboard()
-            self.keyboard_enabled = True
-            self.typing_mode_enabled = True
-            keyboard.restore_state([]) # Release any held keys
+        if text is None:
+            text = self.text
 
-    def press_disable(self, key_name):
-        if self.keyboard_enabled and key_name == self.keyboard_key:
-            # Disable keyboard
-            if self.typing_mode_enabled:
-                keyboard.unhook(self.input_hook)
-            self.disable_keyboard()
-            self.keyboard_enabled = False
-            self.typing_mode_enabled = False
-            return True
-        return False
-        
-    def press_key(self, key_name):
-        if self.press_disable(key_name):
-            return
+        if text != "":
+            print(f"[CHAT] {datetime.datetime.fromtimestamp(time.time()).strftime('[%H:%M:%S]')} {text}")
 
-        if self.keyboard_enabled:
-            if key_name == self.typing_mode_key and self.typing_mode_enabled:
-                # Disable typing mode
-                self.typing_mode_enabled = False
-                keyboard.unhook(self.input_hook)
-            elif key_name == self.mirror_key:
-                # Toggle mirror
-                self.mirror_enabled = not self.mirror_enabled
-                self.enable_keyboard()
-            elif key_name in self.key_mapping.keys():
-                # Typing
-                osc_path = self.osc_prefix + self.key_params[0]
-                self.client.send_message(osc_path, self.key_mapping[key_name])
-                time.sleep(self.key_delay)
-                self.client.send_message(osc_path, 0)
+    def send_text(self, text):
+        self.client.send_message(self.osc_chatbox_input, [text, self.send_immediate])
+        return
 
-                # Shift keyboard message paths array
-                self.key_params.append(self.key_params.pop(0))
+    def send_status(self, typing=False):
+        self.client.send_message(self.osc_chatbox_typing, typing)
+        return
 
-    def handle_inputs(self, event):
-        if self.is_correct_window() and event.event_type == "down":
-            self.press_key(event.name)
+    def handle_socket(self):
+        app = Flask(__name__)
+        socketio = SocketIO(app, logger=False, cors_allowed_origins='*')
+        self.socketio = socketio
 
-    def handle_enable(self, event):
-        if self.is_correct_window() and event.event_type == "down":
-            self.press_enable(event.name)
+        @socketio.on("status")
+        def on_status(status):
+            print(f"[INFO] Status: {status}")
+            pass
 
-    def start(self, announce=True):
-        if announce:
-            print("Switch to the {0} window!".format(self.window))
-        self.enabler_hook = keyboard.hook(self.handle_enable)
+        @socketio.on("speech")
+        def on_speech(speech, translated_speech, untranslated_speech, input_lang, output_lang, translate_enabled, low_latency_enabled, tts_enabled, interim_addition=False, pad_spacing=False):
+            #print(input_lang, output_lang, translate_enabled, low_latency_enabled, tts_enabled, interim_addition, pad_spacing)
+            #print("[INFO] Speech: " + speech)
+
+            if interim_addition and pad_spacing:
+                self.send_text(" " + speech)
+            else:
+                self.send_text(speech)
+            self.print_transcript_line(text=speech)
+
+        @socketio.on("connect")
+        def on_connect():
+            print(f"[INFO] Client connected: {request.sid}")
+            pass
+
+        @socketio.on("disconnect")
+        def on_disconnect():
+            print(f"[INFO] Client disconnected: {request.sid}")
+            pass
+
+        socketio.run(app, host=self.socket_ip, port=self.socket_port)
+
+    def start(self):
+        print("[INFO] OSC Keyboard has started!")
+        #print(f"[INFO] Switch to the {self.window} window!")
+
+        if self.socket_connection:
+            self.socket_thread = SingleThread(self.handle_socket, self.socket_stop_flag)
+            self.socket_thread.start()
+            self.socket_stop_flag = Event()
+
         keyboard.wait()
 
     def stop(self):
-        keyboard.unhook(self.enabler_hook)
+        self.socket_thread = None
+        self.socket_stop_flag.set()
 
 if __name__ == "__main__":
     osc_keyboard = OSCKeyboard(config_file="config.json")
     print("\n".join([
-        "OSC Keyboard by ShadowForest",
+        "OSC Keyboard [v{0}] by ShadowForest".format(__version__),
         "https://github.com/ShadowForests/OSCKeyboard",
         "-----------------------------------------------------------",
-        "Recommended Keyboard: KillFrenzy's VRC Avatar Keyboard",
-        "https://github.com/killfrenzy96/KillFrenzyVRCAvatarKeyboard",
-        "-----------------------------------------------------------",
         "[How To Use]",
-        "- Edit config such as key delay and mappings in config.json",
+        "- View and edit all configurable options inside config.json",
         "- Enable OSC in the VRChat Action Menu under Options -> OSC",
-        "- Press `{0}` to toggle the keyboard on and off".format(osc_keyboard.keyboard_key.upper()),
-        "- While the keyboard is enabled...",
-        "    - Type to write - inputs are buffered to help with sync",
-        "    - Supported input: A-Z, 0-9, spacebar, backspace, enter",
-        "    - Press `{0}` to clear out the current displayed text".format(osc_keyboard.enter_key.upper()),
-        "    - Press `{0}` to mirror text to either face you or others".format(osc_keyboard.mirror_key.upper()),
-        "    - Press `{0}` to toggle typing mode to allow movement and".format(osc_keyboard.typing_mode_key.upper()),
-        "      other actions while keeping text and keyboard visible",
+        # "- Press `{0}` to enable your keyboard, press `{1}` to disable".format(osc_keyboard.enable_keyboard_key.upper(), osc_keyboard.disable_keyboard_key.upper()),
         "-----------------------------------------------------------"
     ]))
     osc_keyboard.start()
--- README.md
+++ README.md
@@ -1,50 +1,52 @@
 # OSC Keyboard
-OSC Keyboard is a program for Windows that utilizes VRC's avatar OSC capabilities and KillFrenzy's VRC Avatar Keyboard to allow users to type in VRChat.
+OSC Keyboard is a program for Windows that utilizes VRC's avatar OSC capabilities and KillFrenzy Avatar Text to allow users to type in VRChat.
 
 ![OSCKeyboardDemo](OSCKeyboardDemo.gif)
 
 ## Requirements
 - Windows is required for foreground window detection
-- Your avatar must have [KillFrenzy's VRC Avatar Keyboard](
-https://github.com/killfrenzy96/KillFrenzyVRCAvatarKeyboard) on it.
-    - This package is not currently working by itself, however, **it does work with OSC**
-    - Follow the provided setup, but you can skip any setup relating to gestures, colliders, and actions menu setup as all input will be handled using parameters and OSC
-    - Things you can skip adding or delete include:
-        - `Trigger Left`, `Trigger Right`, `Keyboard Collider Left`, `Keyboard Collider Right` layers in the provided animator controllers `VRCAC_KeyboardController_Action` and `VRCAC_KeyboardController_FX`
-        - `VRCEM_KeyboardMenu` and `VRCEM_KeyboardSubMenu`
-- `pip install -r requirements.txt` and run the python program
-- Alternatively download the executable in [releases](https://github.com/ShadowForests/OSCKeyboard/releases)
+- Your avatar must have [KillFrenzy Avatar Text](https://github.com/killfrenzy96/KillFrenzyAvatarText) on it
+- Download and run the executable in [releases](https://github.com/ShadowForests/OSCKeyboard/releases)
+- Alternatively `pip install -r requirements.txt` and run the python program
 
 ## How To Use
 - Edit config such as key delay and mappings in config.json
 - Enable OSC in the VRChat Action Menu under Options -> OSC
-- Press `=` to toggle the keyboard on and off
-- While the keyboard is enabled:
-    - Type to write - inputs are buffered to help with sync
+- Press `=` to enable your keyboard, press `ESC` to disable
+- While the keyboard is enabled...
+    - Type to write - inputs will be handled to ensure sync
     - Supported input: A-Z, 0-9, spacebar, backspace, enter
     - Press `ENTER` to clear out the current displayed text
-    - Press `]` to mirror text to either face you or others
-    - Press `-` to toggle typing mode to allow movement and
-      other actions while keeping text and keyboard visible
+    - Press `F1` to turn on or off typing mode to allow for
+      other actions while keeping the existing text visible
+    - Copy, cut, paste, and control+backspace are supported
+    - Japanese hiragana and katakana input is also possible
 
 - OSC Keyboard is only active when you are focused on the VRChat window, unless the `require_focus` config option is `false`
-- The visual keyboard is non-functional and only present as part of the keyboard prefab
-- Only OSC events are sent to VRChat when the keyboard and typing mode are enabled
-- All other keyboard inputs to the game (such as walking or opening menus) are disabled to allow ease of typing
-- If you have sync issues with other players, you can adjust the `key_delay` config option
+- OSC events are sent to VRChat when the keyboard and typing mode are enabled and in use
+- All other inputs to the game (such as walking or opening menus) are disabled to allow ease of typing
+- If you have sync issues with other players, you can try increasing the `key_delay` config option
 
 ## Config Parameters
 These parameters can be adjusted inside `config.json`, which must be in the same directory as the program. This can also potentially allow usage of this program with other custom avatar keyboards.
 - `ip`: OSC IP
 - `port`: OSC Port
+- `realtime_input`: Whether to enable instant feedback typing with delayed sync or use buffered text input
 - `require_focus`: Require focusing on the window for inputs
 - `window`: Target window to restrict inputs in
-- `key_delay`: Delay between key presses
-- `keyboard_key`: Key to toggle keyboard
+- `key_delay`: Delay between key presses - setting this below 0.25 seconds may increase sync issues for other players
+- `sync_param_count`: Amount of parameters to use for syncing text
+- `transcript`: Enables a transcript log of keyboard messages
+- `auto_format_text`: Enable some automatic text formatting such as normalizing unicode characters or splitting dakuten and handakuten from katakana
+- `socket_connection`: Enables a socket server connection to allow external input to the keyboard
+- `socket_ip`: Socket IP
+- `socket_port`: Socket Port
+- `enable_keyboard_key`: Key to enable keyboard
+- `disable_keyboard_key`: Key to disable keyboard
 - `typing_mode_key`: Key to toggle typing mode
-- `mirror_key`: Key to toggle text mirror
-- `enter_key`: Key to clear text
 - `key_mapping`: Mapping of key to parameter value
+- `max_length`: Max length of keyboard display
 - `osc_prefix`: OSC path prefix
 - `keyboard_param`: OSC keyboard parameter name
+- `pointer_param`: OSC pointer parameter name
 - `key_params`: OSC key parameter names
